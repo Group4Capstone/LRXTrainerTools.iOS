3fbc94775d8dcdcff0bee8ead8991037f575dcb9
Bryan Williams <bjw35@students.uwf.edu>
2015-11-26 01:51:20 -0600

    basic iOS UI

---
+++ LRXTrainerTools.iOS/SecondViewController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+using UIKit;
+
+namespace LRXTrainerTools.iOS
+{
+	public partial class SecondViewController : UIViewController
+	{
+		public SecondViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();	
+			// Perform any additional setup after loading the view, typically from a nib.
+			Stopwatch stopwatch = new Stopwatch ();
+
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
---
+++ LRXTrainerTools.iOS/obj/iPhoneSimulator/Debug/__XamarinInsightsApiKeyClass.ide.cs
@@ -0,0 +1,7 @@
+namespace LRXTrainerTools.iOS
+{
+	class XamarinInsights
+	{
+		public const string ApiKey = "664122f024ab41c2de025f314faf8fef50d9e418";
+	}
+}
